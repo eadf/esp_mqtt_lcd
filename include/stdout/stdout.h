77fdbb0cae5e008706caa974d834971644b5bc8e
EAD Fritz <ead.fritz@gmail.com>
2015-02-16 11:36:05 +0100

    Squashed 'driver/stdout/' content from commit 63b400a
    git-subtree-dir: driver/stdout
    git-subtree-split: 63b400ae8561b9a373dd0379adc34fbf27cd8344

---
+++ include/stdout/stdout.h
@@ -0,0 +1,12 @@
+//Stupid bit of code that does the bare minimum to make os_printf work.
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Jeroen Domburg <jeroen@spritesmods.com> wrote this file. As long as you retain
+ * this notice you can do whatever you want with this stuff. If we meet some day,
+ * and you think this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
+void stdout_init(void);
